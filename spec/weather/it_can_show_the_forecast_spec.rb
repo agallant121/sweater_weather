19618190d0f5a7a6e10359d28626d62796c86ad7
Alex Gallant <agallant1201@gmail.com>
2020-04-19 17:29:49 -0600

    Edit spacing

--- spec/weather/it_can_show_the_forecast_spec.rb
+++ spec/weather/it_can_show_the_forecast_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'weather services' do
   it 'can display lat and long' do
     weather_info = Weather.new('Denver')
-    
+
     expect(weather_info.current_weather.count.class).to be(Integer)
     expect(weather_info.future_daily_forecast.count.class).to be(Integer)
     expect(weather_info.hourly_weather.count.class).to be(Integer)
