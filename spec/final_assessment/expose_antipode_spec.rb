c7001f60c0567bb7ed08adb7cc904c18842d8883
Alex Gallant <agallant1201@gmail.com>
2020-04-20 11:52:18 -0600

    Set up spec and begin calling for exposed api

---
+++ spec/final_assessment/expose_antipode_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user', type: :request do
+  it "gets weather when called for a location" do
+    location = 'Hong Kong'
+
+    get "/api/v1/antipode?location=#{location}"
+
+    expect(response).to be_successful
+  end
+end
