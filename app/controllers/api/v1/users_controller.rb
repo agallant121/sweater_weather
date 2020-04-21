57229602f5a575c4010aec41ea59306f78439c02
Alex Gallant <agallant1201@gmail.com>
2020-04-20 22:18:10 -0600

    Move pry

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    # require "pry"; binding.pry
     user = User.new(user_params)
     # require "pry"; binding.pry
     if user.save
