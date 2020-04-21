27f89784977ac5af2500c5896917fb94aebf4963
Alex Gallant <agallant1201@gmail.com>
2020-04-21 10:24:51 -0600

    Add before each and test for sad path to increase covergae to 100%

--- spec/requests/api/v1/can_plan_roatrip_spec.rb
+++ spec/requests/api/v1/can_plan_roatrip_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'As a user', type: :request do
+  before :each do
+    User.destroy_all
+  end
+
   it 'can show roadtrip details' do
 
     post '/api/v1/users?email=agallant121@gmail.com&password=penny&password_confirmation=penny'
@@ -24,5 +28,19 @@ RSpec.describe 'As a user', type: :request do
     expect(result[:travel_duration_text]).to eq("1 hour 48 mins")
     expect(result[:future_forecast_temperature].class).to be(Float)
     expect(result[:future_forecast_description].class).to be(String)
+  end
+
+  it 'unsuccessful response due to unauthorized user' do
+    post '/api/v1/users?email=agallant121@gmail.com&password=penny&password_confirmation=penny'
+    api_key = JSON.parse(response.body, symbolize_names: true)[:data][:attributes][:api_key]
+
+    post('/api/v1/road_trip', params: {
+                            origin: "Denver,CO",
+                            destination: "Pueblo,CO",
+                            api_key: "#{api_key}456" })
+
+    expect(response.status).to eq(401)
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(result[:message]).to eq('Invalid Key')
   end
 end
