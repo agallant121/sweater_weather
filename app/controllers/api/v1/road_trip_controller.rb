589490240a923d220bded695a9fdc604664c8873
Alex Gallant <agallant1201@gmail.com>
2020-04-20 22:18:20 -0600

    Edit rpad trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    render json: RoadtripSerializer.new(Roadtrip.new(params[:origin],
-                                                     params[:destination],
-                                                     params[:api_key][:api_key]))
+    # require "pry"; binding.pry
+    user = User.find_by(auth_token: params[:api_key])
+    if user
+      road_trip = Roadtrip.new(params[:origin], params[:destination])
+      render json: RoadtripSerializer.new(road_trip)
+    else
+      render json: {message: 'Invalid Key'}, status: :unauthorized
+    end
   end
 end
