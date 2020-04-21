96f55b8429ed76258f2b869afce015cedfbc9824
Alex Gallant <agallant1201@gmail.com>
2020-04-20 22:17:49 -0600

    Add methods and remove methods

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -1,22 +1,16 @@
 class Roadtrip
-  attr_reader :id, :road_trip, :origin, :destination, :future_forecast_temperature, :future_forecast_description
+  attr_reader :id, :origin, :destination, :future_forecast_temperature, :future_forecast_description, :travel_duration_text
 
-  def initialize(origin, destination, api_key)
+  def initialize(origin, destination)
     @id = nil
     @origin = origin
     @destination = destination
     @connection ||= GoogleMapService.new(origin, destination).connection
-    @api_key = "api_key: #{api_key}"
-    @road_trip = @origin, @destination, @api_key
   end
 
   def travel_duration_text
     get_json_maps[:routes][0][:legs][0][:duration][:text]
   end
-
-  # def get_destination_weather
-  #   Weather.new(@destination).hourly_weather[0][:weather][0][:description]
-  # end
 
   def future_time
     travel_duration_text.split.first.to_i + 1
