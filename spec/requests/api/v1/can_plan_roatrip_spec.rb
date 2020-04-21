10aeb7ab394be2d07cc8bde55685235184a3dbf6
Alex Gallant <agallant1201@gmail.com>
2020-04-20 22:17:11 -0600

    Edit roadtrip spec

--- spec/requests/api/v1/can_plan_roatrip_spec.rb
+++ spec/requests/api/v1/can_plan_roatrip_spec.rb
@@ -5,24 +5,31 @@ RSpec.describe 'As a user', type: :request do
 
     post '/api/v1/users?email=agallant121@gmail.com&password=penny&password_confirmation=penny'
     post '/api/v1/sessions?email=agallant121@gmail.com&password=penny'
-    api_key = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+    api_key = JSON.parse(response.body, symbolize_names: true)[:data][:attributes][:api_key]
     # require "pry"; binding.pry
 
-    origin = 'Denver, CO'
-    destination = 'Pueblo, CO'
-
+    origin = 'Denver,CO'
+    destination = 'Pueblo,CO'
+    # require "pry"; binding.pry
     post '/api/v1/road_trip', params: {
        origin: origin,
        destination: destination,
        api_key: api_key
      }
 
-     roadtrip_info = Roadtrip.new('Denver, CO', 'Pueblo, CO', api_key)
+     # roadtrip_info = Roadtrip.new('Denver, CO', 'Pueblo, CO', api_key)
 
     expect(response.status).to eq(200)
-    expect(roadtrip_info.travel_duration_text).to eq("1 hour 48 mins")
-    expect(roadtrip_info.future_forecast_temperature.class).to be(Float)
-    expect(roadtrip_info.future_forecast_description.class).to be(String)
-    # expect(roadtrip_info.get_destination_weather.class).to be(String)
+
+    result = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+    expect(result[:origin]).to eq("Denver,CO")
+    expect(result[:destination]).to eq("Pueblo,CO")
+    expect(result[:travel_duration_text]).to eq("1 hour 48 mins")
+    expect(result[:future_forecast_temperature].class).to be(Float)
+    expect(result[:future_forecast_description].class).to be(String)
+    # require "pry"; binding.pry
+    # expect(roadtrip_info.travel_duration_text).to eq("1 hour 48 mins")
+    # expect(roadtrip_info.future_forecast_temperature.class).to be(Float)
+    # expect(roadtrip_info.future_forecast_description.class).to be(String)
   end
 end
